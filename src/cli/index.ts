1f3b5d5838253d16fe7a33d83139ac0a07fdd2e3
Omar Aizab <omaraizab98@gmail.com>
2024-04-23 21:53:42 +0100

    add count option and searchstring from file

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -20,12 +20,25 @@ yargs(hideBin(process.argv))
         .option('view', {
           describe: 'This alias represents the list of elements that will be returned in the response.',
           choices: ['STANDARD', 'COMPLETE'],
+          default: 'STANDARD',
+        })
+        .option('count', {
+          describe: 'Count of the search results',
+          type: 'boolean',
+        })
+        .option('searchstringfromfile', {
+          describe: 'Search string read from file.',
+          type: 'string',
         });
     },
   )
   .middleware(async (argv) => {
     if (!argv.searchQuery) {
       console.error('Search query is required');
+      process.exit(1);
+    }
+    if (!argv.searchstring && !argv.searchstringfromfile) {
+      console.log('Please provide a search string (positional args) or use --searchstringfromfile=file.txt.');
       process.exit(1);
     }
     await search(argv);
