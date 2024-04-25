e6ddfd6ee42587e95392309b8480e2ce1e246cca
Omar Aizab <omaraizab98@gmail.com>
2024-04-25 19:56:18 +0100

    update cli
    add autosave
    add save config json file
    add search from file
    add support to key... to search from key file
    add chunking

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -17,6 +17,14 @@ yargs(hideBin(process.argv))
           describe: 'Fetch all pages of the search results',
           type: 'boolean',
         })
+        .option('title', {
+          describe: 'Search only in title',
+          type: 'boolean',
+        })
+        .option('title_and_abstract', {
+          describe: 'Search only in title and abstract',
+          type: 'boolean',
+        })
         .option('view', {
           describe: 'This alias represents the list of elements that will be returned in the response.',
           choices: ['STANDARD', 'COMPLETE'],
@@ -26,9 +34,27 @@ yargs(hideBin(process.argv))
           describe: 'Count of the search results',
           type: 'boolean',
         })
+        .option('perPage', {
+          describe: 'Number of items per page',
+          type: 'number',
+        })
+        .option('chunkSize', {
+          describe:
+            'this option only works with --allpages. It will retrieve the results in chunks of the specified size. E.g. --chunkSize=100 will retrieve 100 results at a time. and save them to files in a folder with same name in --save option,',
+          type: 'number',
+        })
+        .option('time', {
+          describe: 'Time of the search',
+          type: 'boolean',
+          default: true,
+        })
         .option('searchstringfromfile', {
           describe: 'Search string read from file.',
           type: 'string',
+        })
+        .option('autosave', {
+          describe: 'Save the search results to a json file with the search string as the filename',
+          type: 'boolean',
         });
     },
   )
@@ -39,6 +65,17 @@ yargs(hideBin(process.argv))
     }
     if (!argv.searchQuery && !argv.searchstringfromfile) {
       console.log('Please provide a search string (positional args) or use --searchstringfromfile=file.txt.');
+      process.exit(1);
+    }
+    if (argv.title && argv.title_and_abstract) {
+      console.log('Please provide only one search field --title or --title_and_abstract.');
+      process.exit(1);
+    }
+    if (argv.save && argv.time) {
+      argv.time = false;
+    }
+    if (argv.save && argv.autosave) {
+      console.log('Please provide only one of --save or --autosave.');
       process.exit(1);
     }
     await search(argv);
--- src/cli/utils/parser.ts
+++ src/cli/utils/parser.ts
@@ -7,6 +7,80 @@ function getKey() {
   return key.trim();
 }
 
+function sanitise(str: string) {
+  let term = str;
+  term = term.replace(/\t+/gs, ' ');
+  term = term.replace(/ +/gs, ' ');
+  term = term.replace(/^ +/gs, '');
+  term = term.replace(/ +$/gs, '');
+  return term;
+}
+
+function quoteIfNeeded(term: string) {
+  let t = term;
+  if (t.match(/ /) && !t.match(/^".*"$/)) {
+    t = `"${t}"`;
+  }
+  return t;
+}
+
+function searchBuilder(query: any) {
+  let searchQuery = '';
+
+  for (let i = 0; i < query.length; i += 1) {
+    if (query[i].match(/(\w+)\.\.\./)) {
+      const key = query[i].match(/(\w+)\.\.\./)[1];
+      // open a file
+      const file = `searchterms/${key}.txt`;
+      let result = fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : key;
+      // split result into an array by new line
+      const resultarr = result.split(/\r?\n/);
+      result = '';
+      let operator = '';
+      let useoperator = false;
+      // remove comments from results file
+      for (let j = 0; j < resultarr.length; j += 1) {
+        if (resultarr[j].match(/#(OR|AND)\s*$/)) {
+          operator = `${resultarr[j].match(/#(OR|AND)\s*$/)[1]} `;
+          useoperator = true;
+        }
+        if (resultarr[j].match(/#(-)\s*$/)) {
+          useoperator = true;
+          operator = ' ';
+          console.log('operator empty.');
+        }
+        const term = sanitise(resultarr[j].replace(/#.+$/g, ''));
+        if (term !== '') {
+          result
+            += `${(result.match(/[\w")]\s+$/) && !term.match(/^\s*\)/) ? operator : '')
+            + (useoperator ? quoteIfNeeded(term) : term)
+            } `;
+        }
+      }
+      result = query[i].replace(RegExp(`${key}\\.\\.\\.`), result);
+      searchQuery += ` ${result}`;
+    } else {
+      searchQuery += ` ${quoteIfNeeded(query[i])}`;
+    }
+  }
+  // Allow use of [ and ] instead of ( and ).
+  searchQuery = searchQuery.replace(/\[/gs, '(');
+  searchQuery = searchQuery.replace(/\]/gs, ')');
+  return searchQuery;
+}
+
+function buildQuery(query: any, title?:boolean, titleAndAbstract?:boolean) {
+  let searchQuery = searchBuilder(query);
+  if (title) {
+    searchQuery = `TITLE(${searchQuery})`;
+  }
+  if (titleAndAbstract) {
+    searchQuery = `TITLE-ABS-KEY(${searchQuery})`;
+  }
+  console.log('Final query:', searchQuery);
+  return searchQuery;
+}
+
 async function saveAndSearch(scopusOptions: ScopusSearchRequest) {
   fs.writeFileSync('scopusOptions.json', JSON.stringify(scopusOptions, null, 2));
   const scopusSDK = new ScopusSDK(getKey());
@@ -14,16 +88,69 @@ async function saveAndSearch(scopusOptions: ScopusSearchRequest) {
   return res;
 }
 
+async function handleCount(scopusOptions: ScopusSearchRequest) {
+  scopusOptions.perPage = 1;
+  scopusOptions.page = 1;
+  const results = await saveAndSearch(scopusOptions);
+  console.log('count:', results.data['search-results']['opensearch:totalResults']);
+  return results.data['search-results']['opensearch:totalResults'];
+}
+
 export default async function search(args: any) {
+  let query = args.searchQuery;
+  if (args.searchstringfromfile) {
+    if (!fs.existsSync(args.searchstringfromfile)) {
+      console.log(`File not found: ${args.searchstringfromfile}`);
+      process.exit(1);
+    }
+    query = fs.readFileSync(args.searchstringfromfile, 'utf8');
+    query = query.split(/\r?\n/);
+  }
   const scopusOptions: ScopusSearchRequest = {
-    query: args.searchQuery.join(' '),
+    query: buildQuery(query, args.title, args.title_and_abstract).trim(),
   };
+  if (args.count) {
+    const res = await handleCount(scopusOptions);
+    return res;
+  }
+  if (args.view) {
+    scopusOptions.view = args.view;
+  }
+
   if (args.save) {
     scopusOptions.toJson = args.save;
   }
 
+  if (args.autosave) {
+    const filename = scopusOptions.query?.trim().split('  ').join(' ');
+    scopusOptions.toJson = filename;
+
+    if (args.time) {
+      // Y-M-D_H:M:S
+      const date = new Date().toISOString().replace('T', '_').replace(/\..+/, '');
+      scopusOptions.toJson = `${date}-${filename}`;
+    }
+    args.save = scopusOptions.toJson;
+  }
+
+  if (args.perPage) {
+    scopusOptions.perPage = args.perPage;
+  }
+
+  if (args.chunkSize) {
+    scopusOptions.chunkSize = args.chunkSize;
+  }
   scopusOptions.retriveAllPages = !!args.allpages;
 
   const res = await saveAndSearch(scopusOptions);
-  console.log(res);
+  if (args.save || args.autosave) {
+    const configJson = {
+      date: new Date().toISOString().replace('T', ' ').replace(/\..+/, ''),
+      ...scopusOptions,
+    };
+    fs.writeFileSync(`${scopusOptions.toJson}.config.json`, JSON.stringify(configJson, null, 2));
+  } else {
+    console.log(JSON.stringify(res.data, null, 2));
+  }
+  return res.data;
 }
