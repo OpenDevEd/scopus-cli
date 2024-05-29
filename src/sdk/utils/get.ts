1161c3ba574a1b41711058cbc20ea8556228e58e
Aizab Omar <omaraizab98@gmail.com>
2024-05-29 10:46:32 +0100

    extend timeout

--- src/sdk/utils/get.ts
+++ src/sdk/utils/get.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
 import { ScopusSearchResponse } from '../types/scopusSearchResponse';
 
+const httpClient = axios.create()
+httpClient.defaults.timeout = 60000
+
 async function GET(
   url: string,
   headers: Record<string, string>,
   queryParams: Record<string, string>,
 ): Promise<AxiosResponse<ScopusSearchResponse>> {
   try {
-    const response = await axios.get(url, {
+    const response = await httpClient.get(url, {
       headers,
       params: queryParams,
     });
